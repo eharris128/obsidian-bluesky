82a25a4d98f7ae20fea247f2af296574afbdaff2
Evan Harris <echarris@smcm.edu>
2024-11-10 11:22:39 -0500

    Rm modal & other unused code

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 import { createBlueskyPost } from '@/bluesky';
 import { BlueskyTab } from '@/views/BlueskyTab';
-import { BLUESKY_TITLE, VIEW_TYPE_TAB, VIEW_TYPE_SIDEBAR } from '@/consts';
+import { BLUESKY_TITLE, VIEW_TYPE_TAB } from '@/consts';
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
@@ -32,43 +32,6 @@ export default class MyPlugin extends Plugin {
 
     async onload() {
         await this.loadSettings();
-
-        // This adds a simple command that can be triggered anywhere
-        this.addCommand({
-            id: 'open-sample-modal-simple',
-            name: 'Open sample modal (simple)',
-            callback: () => {
-                new SampleModal(this.app).open();
-            }
-        });
-        // This adds an editor command that can perform some operation on the current editor instance
-        this.addCommand({
-            id: 'sample-editor-command',
-            name: 'Sample editor command',
-            editorCallback: (editor: Editor, view: MarkdownView) => {
-                console.log(editor.getSelection());
-                editor.replaceSelection('Sample Editor Command');
-            }
-        });
-        // This adds a complex command that can check whether the current state of the app allows execution of the command
-        this.addCommand({
-            id: 'open-sample-modal-complex',
-            name: 'Open sample modal (complex)',
-            checkCallback: (checking: boolean) => {
-                // Conditions to check
-                const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-                if (markdownView) {
-                    // If checking is true, we're simply "checking" if the command can be run.
-                    // If checking is false, then we want to actually perform the operation.
-                    if (!checking) {
-                        new SampleModal(this.app).open();
-                    }
-
-                    // This command will only show up in Command Palette when the check function returns true
-                    return true;
-                }
-            }
-        });
 
         // Add new command to post to Bluesky
         // In the onload() method, update the post-to-bluesky command:
@@ -139,22 +102,6 @@ export default class MyPlugin extends Plugin {
             type: VIEW_TYPE_TAB,
             active: true
         });
-    }
-}
-
-class SampleModal extends Modal {
-    constructor(app: App) {
-        super(app);
-    }
-
-    onOpen() {
-        const { contentEl } = this;
-        contentEl.setText('Woah!');
-    }
-
-    onClose() {
-        const { contentEl } = this;
-        contentEl.empty();
     }
 }
 
