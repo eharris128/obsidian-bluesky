a19fc49a93e750e4ddff500f0336b23cb137a80f
Evan Harris <echarris@smcm.edu>
2025-06-16 22:33:04 -0500

    Address typing complaint

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -9,31 +9,31 @@ class Logger {
         this.isDev = typeof __DEV__ !== 'undefined' && __DEV__;
     }
 
-    log(...args: any[]): void {
+    log(...args: unknown[]): void {
         if (this.isDev) {
             console.log('[Bluesky]', ...args);
         }
     }
 
-    warn(...args: any[]): void {
+    warn(...args: unknown[]): void {
         if (this.isDev) {
             console.warn('[Bluesky]', ...args);
         }
     }
 
-    error(...args: any[]): void {
+    error(...args: unknown[]): void {
         if (this.isDev) {
             console.error('[Bluesky]', ...args);
         }
     }
 
-    debug(...args: any[]): void {
+    debug(...args: unknown[]): void {
         if (this.isDev) {
             console.debug('[Bluesky]', ...args);
         }
     }
 
-    info(...args: any[]): void {
+    info(...args: unknown[]): void {
         if (this.isDev) {
             console.info('[Bluesky]', ...args);
         }
